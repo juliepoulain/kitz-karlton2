a74e5a1f8797cbcc2df1878ae83a25528980620c
Hollis Crowder <hollis.crowder@gmail.com>
2024-05-03 15:59:37 -0700

    reconfigure cats to have owner ids

--- lib/debug.py
+++ lib/debug.py
@@ -5,6 +5,13 @@ from models.owner import Owner
 import ipdb
 from models.cat import Cat
 
+Owner.drop_table()
+Owner.create_table()
+Hollis = Owner.create("Hollis", 7202337895, "274A 5th Ave, San Francisco CA 94118")
+Cat.drop_table()
+Cat.create_table()
+Higgs = Cat.create("Higgs", "shorthair", 1, 2, Hollis.id)
 
+Higgs.owner()
 # test
 ipdb.set_trace()
