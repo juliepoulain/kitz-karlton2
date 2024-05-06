a0c8d734944cfba6d2b9093503ee56d6ec07f034
Julie Poulain <juliempoulain@gmail.com>
2024-05-06 10:46:04 -0700

    add basic orm structure to reservation class

--- lib/debugjulie.py
+++ lib/debugjulie.py
@@ -3,15 +3,13 @@
 
 from models.owner import Owner
 from models.cat import Cat
+from models.reservation import Reservation
 import ipdb
 
-Owner.drop_table()
-Owner.create_table()
-test_owner1 = Owner.create("new", 9785510848, "new")
-test_owner2 = Owner.create("julie", 9785510848, "test2")
-Cat.drop_table()
-Cat.create_table()
-test_cat1 = Cat.create("Olivia", "calico", 2, 2, test_owner2.id)
-print(Owner.get_cats(test_owner2))
+Reservation.drop_table()
+Reservation.create_table()
+test_res1 = Reservation.create(9785510848, 1, 2)
+test_res1.phone_number = 9785510847
+test_res1.update()
 
 ipdb.set_trace()
