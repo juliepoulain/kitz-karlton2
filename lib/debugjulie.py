b4cfdf605654af0b0e776fa28d4572f24aef2bb5
Julie Poulain <juliempoulain@gmail.com>
2024-05-07 09:49:11 -0700

    push file updates

--- lib/debugjulie.py
+++ lib/debugjulie.py
@@ -8,6 +8,7 @@ import ipdb
 
 Reservation.drop_table()
 Reservation.create_table()
+Reservation.get_all()
 test_res1 = Reservation.create(9785510848, 1, 2)
 test_res1.phone_number = 9785510847
 test_res1.update()
