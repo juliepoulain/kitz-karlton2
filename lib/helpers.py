d55fdb2a6c07b3a406621a8664d1ce01a1e52d92
Julie Poulain <juliempoulain@gmail.com>
2024-05-07 15:05:17 -0700

    finish owner helpers

--- lib/helpers.py
+++ lib/helpers.py
@@ -82,13 +82,15 @@ def employee_manage_res():
     elif choice == "5":
         update_reservation()
 
-
 def employee_manage_owner():
-    print("Owner data:")
-    print(Owner.get_all())
+    print("Menu Options:")
     print("0: Exit Program")
     print("1: Return to Main Menu")
     print("2: Return to Employee Portal")
+    print("3: See all Owners")
+    print("4: Create Owner")
+    print("5: Update Owner")
+    print("6: View specific Owner")
     choice = input("> ")
     if choice == "0":
         exit_program()
@@ -96,6 +98,15 @@ def employee_manage_owner():
         return_main_menu()
     elif choice == "2":
         employee_portal()
+    elif choice == "3":
+        print(Owner.get_all())
+        employee_manage_owner()
+    elif choice == "4":
+        create_owner()
+    elif choice == "5":
+        update_owner()
+    elif choice == "6":
+        specific_owner()
 
 
 def employee_manage_cat():
@@ -448,7 +459,160 @@ def check_cat_owner(name, breed, age, spice_level):
 
 
 
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # julie works here
 def create_reservation():
     print("Please enter valid phone number")
@@ -486,15 +650,15 @@ def check_hotel_room_number(phone_number, length_of_stay):
         and 0 < int(hotel_room_number) < 11
     ):
         print("Thank you for making a reservation!")
+        hotel_room_number = int(hotel_room_number)
+        Reservation.create(phone_number, length_of_stay, hotel_room_number)
         final_res_create(phone_number, length_of_stay, hotel_room_number)
     else:
         print("INVALID: Hotel Room Number must be an integer between 1-10")
         check_hotel_room_number(phone_number, length_of_stay)
 
 def final_res_create(phone_number, length_of_stay, hotel_room_number):
-    hotel_room_number = int(hotel_room_number)
     print("Reservation Details:")
-    new_reservation = Reservation.create(phone_number, length_of_stay, hotel_room_number)
     print(Reservation.find_by_phone(phone_number))
     print("Returning to menu options...")
     time.sleep(1)
@@ -503,8 +667,8 @@ def final_res_create(phone_number, length_of_stay, hotel_room_number):
     print("1: Return to Main Menu")
     print("2: Return to Employee Portal")
     print("3: Create another reservation")
-    print("4: Update this Reservation")
-    print("5: Delete this Reservation")
+    print("4: Update selected Reservation")
+    print("5: Delete selected Reservation")
     choice = input("> ")
     if choice == "0":
         exit_program()
@@ -561,6 +725,10 @@ def specify_reservation_update(res_to_update):
         enter_new_hotel_room(res_to_update)
     elif choice == "6":
         delete_selected_reservation(res_to_update)
+    else:
+        print("INVALID: entered value must be a number from given menu options")
+        specify_reservation_update(res_to_update)
+        
 
 def enter_new_phone(res_to_update):
     print(f"Selected Reservation: {res_to_update}")
@@ -643,3 +811,222 @@ def delete_selected_reservation(res_to_update):
         employee_manage_res()
     else:
         specify_reservation_update(res_to_update)
+
+def create_owner():
+    print("Please enter owner's name")
+    entered_name = input("> ")
+    if (
+        isinstance(entered_name, str) 
+        and 0 < len(entered_name) <= 20
+    ):
+        check_owner_phone_number(entered_name)
+    else:
+        print("INVALID: Name must be a valid string between 1-20 characters")
+        create_owner()
+
+def check_owner_phone_number(entered_name):
+    print("Please enter owner's phone number")
+    entered_phone = input("> ")
+    if (
+        entered_phone.isnumeric() == True
+        and len(entered_phone) == 10
+    ):
+        entered_phone = int(entered_phone)
+        check_owner_address(entered_name, entered_phone)
+    else:
+        print("INVALID: Phone number must be 10 digits with no spaces")
+        check_owner_phone_number(entered_name)
+
+def check_owner_address(entered_name, entered_phone):
+    print("Please enter owner's address")
+    entered_address = input("> ")
+    if (
+        isinstance(entered_address, str)
+        and 0 < len(entered_address)
+    ):
+        Owner.create(entered_name, entered_phone, entered_address)
+        print("Thank you for creating a new owner!")
+        final_owner_create(entered_name, entered_phone, entered_address)
+    else:
+        print("INVALID: Address must be a string")
+        check_owner_address(entered_name, entered_phone)
+
+def final_owner_create(entered_name, entered_phone, entered_address):
+    print("Owner Details:")
+    print(Owner.find_by_phone(entered_phone))
+    print("Returning to menu options...")
+    time.sleep(1)
+    print("Menu Options:")
+    print("0: Exit Program")
+    print("1: Return to Main Menu")
+    print("2: Return to Employee Portal")
+    print("3: Create another owner")
+    print("4: Update selected owner")
+    print("5: Delete selected owner")
+    print("6: Create a cat for selected owner")
+    choice = input("> ")
+    if choice == "0":
+        exit_program()
+    elif choice == "1":
+        main_menu()
+    elif choice == "2":
+        employee_portal()
+    elif choice == "3":
+        create_owner()
+    elif choice == "4":
+        owner_to_update = Owner.find_by_phone(entered_phone)
+        specify_owner_update(owner_to_update)
+    elif choice == "5":
+        owner_to_update = Owner.find_by_phone(entered_phone)
+        delete_selected_owner(owner_to_update)
+    elif choice == "6":
+        cat_owner = Owner.find_by_phone(entered_phone)
+        create_cat(cat_owner.id)
+    else:
+        print("INVALID: entered value must be a number from given menu options")
+        final_owner_create(entered_name, entered_phone, entered_address)
+        
+def update_owner():
+    print(Owner.get_all())
+    print("Please enter the ID (listed above) for the owner you would like to update")
+    entered_id = input("> ")
+    if (
+        entered_id.isnumeric() and Owner.find_by_id(int(entered_id))
+    ):
+        owner_to_update = Owner.find_by_id(entered_id)
+        specify_owner_update(owner_to_update)
+    else:
+        print("INVALID: ID must be an existing Owner ID")
+        update_owner()
+
+def specify_owner_update(owner_to_update):
+    print(f"Selected Owner: {owner_to_update}")
+    print("0: Exit Program")
+    print("1: Return to Main Menu")
+    print("2: Return to Employee Portal")
+    print("3: Update Name for selected owner")
+    print("4: Update Phone Number for selected owner")
+    print("5: Update Address for selected owner")
+    print("6: Delete selected reservation")
+    choice = input("> ")
+    if choice == "0": 
+        exit_program()
+    elif choice == "1":
+        return_main_menu()
+    elif choice == "2":
+        employee_portal()
+    elif choice == "3":
+        enter_new_owner_name(owner_to_update)
+    elif choice == "4":
+        enter_new_owner_phone(owner_to_update)
+    elif choice == "5":
+        enter_new_owner_address(owner_to_update)
+    elif choice == "6":
+        delete_selected_owner(owner_to_update)
+    else:
+        print("INVALID: entered value must be a number from given menu options")
+        specify_owner_update(owner_to_update)
+
+def enter_new_owner_name(owner_to_update):
+    print(f"Selected Owner: {owner_to_update}")
+    print("Please enter new owner name")
+    new_name = input("> ")
+    if (
+        isinstance(new_name, str) 
+        and 0 < len(new_name) <= 20
+    ):
+        owner_to_update.name = new_name
+        owner_to_update.update()
+        print("Owner Name has been updated successfully!")
+        print("Updated Reservation Details:")
+        print(owner_to_update)
+        print("Continue to update this owner information? Enter Y or N")
+        choice = input("> ")
+        if choice == "Y" or choice == "y":
+            specify_owner_update(owner_to_update)
+        else:
+            employee_manage_owner()
+    else: 
+        print("INVALID: Phone number must be 10 digits with no spaces")
+        enter_new_owner_name(owner_to_update)
+
+def enter_new_owner_phone(owner_to_update):
+    print(f"Selected Owner: {owner_to_update}")
+    print("Please enter new phone number")
+    new_phone = input("> ")
+    if (
+        new_phone.isnumeric() == True
+        and len(new_phone) == 10
+    ):
+        owner_to_update.phone_number = int(new_phone)
+        owner_to_update.update()
+        print("Phone Number has been updated successfully!")
+        print("Updated Reservation Details:")
+        print(owner_to_update)
+        print("Continue to update this owner? Enter Y or N")
+        choice = input("> ")
+        if choice == "Y" or choice == "y":
+            specify_owner_update(owner_to_update)
+        else:
+            employee_manage_owner()
+    else: 
+        print("INVALID: Phone number must be 10 digits with no spaces")
+        enter_new_owner_phone(owner_to_update)
+
+def enter_new_owner_address(owner_to_update):
+    print(f"Selected Owner: {owner_to_update}")
+    print("Please enter new address for selected owner")
+    new_address = input("> ")
+    if (
+        isinstance(new_address, str)
+        and 0 < len(new_address)
+    ):
+        owner_to_update.address = new_address
+        owner_to_update.update()
+        print("Address has been updated successfully!")
+        print("Updated Owner Details:")
+        print(owner_to_update)
+        print("Continue to update this owner? Enter Y or N")
+        choice = input("> ")
+        if choice == "Y" or choice == "y":
+            specify_owner_update(owner_to_update)
+        else:
+            employee_manage_owner()
+    else: 
+        print("INVALID: Address must be string")
+        enter_new_owner_address(owner_to_update)
+
+def delete_selected_owner(owner_to_update):
+    print(f"Selected Owner: {owner_to_update}")
+    print("Are you sure you want to delete this owner? Enter Y or N")
+    choice = input("> ")
+    if choice == "Y" or choice == "y":
+        owner_to_update.delete()
+        print("Owner deleted successfully!")
+        print("Returning to manage owners...")
+        time.sleep(1)
+        employee_manage_owner()
+    else:
+        specify_owner_update(owner_to_update)
+
+
+def specific_owner():
+    print(Owner.get_all())
+    print("Please enter the ID (listed above) for the owner you would like to view")
+    entered_id = input("> ")
+    if (
+        entered_id.isnumeric() and Owner.find_by_id(int(entered_id))
+    ):
+        print(Owner.find_by_id(entered_id))
+        view_owner = Owner.find_by_id(entered_id)
+        print("See this owner's cats? Enter Y or N")
+        choice = input("> ")
+        if choice == "Y" or choice == "y":
+            print(view_owner.get_cats())
+            employee_manage_owner()
+        else:
+            employee_manage_owner()
+    else:
+        print("INVALID: ID must be an existing Owner ID")
+        specific_owner()
+
